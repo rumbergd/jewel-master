87f16b2e2ef65b87788abc07333c8da962c71f67
Dmitry Rumberg <drumberg@cargurus.com>
2025-04-01 10:47:48 -0400

    Initial Commit

---
+++ JewelMaster/JewelMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JewelMasterApp.swift
+//  JewelMaster
+//
+//  Created by Dmitry Rumberg on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct JewelMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JewelMasterTests/JewelMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  JewelMasterTests.swift
+//  JewelMasterTests
+//
+//  Created by Dmitry Rumberg on 4/1/25.
+//
+
+import Testing
+@testable import JewelMaster
+
+struct JewelMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
